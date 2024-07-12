997713f8f294faf5a1e61b89c7f660b2d077ec88
k0rrluna <k0rr.luna1@gmail.com>
2024-07-12 23:57:42 +0300

    verilog

--- labs/lab6/bibp.v
+++ labs/lab6/bibp.v
@@ -1,14 +1,24 @@
 module bibp #(parameter UZUNLUK = 8)(
     input [UZUNLUK + 2:0] buyruk,
-    output [UZUNLUK:0] sonuc
+/*    output reg [(UZUNLUK/2) - 1:0] v1,
+    output reg [(UZUNLUK/2) - 1:0] v2, */
+    output reg [(UZUNLUK/2):0] sonuc
 );
 
-localparam halfUZUNLUK = UZUNLUK/2;
-localparam [halfUZUNLUK - 1 : 0] v1 = buyruk[UZUNLUK - 1: halfUZUNLUK];
-localparam [halfUZUNLUK - 1: 0] v2 = buyruk[halfUZUNLUK - 1 : 0];
+/*    localparam halfUZUNLUK = UZUNLUK / 2;
+    localparam v1 = buyruk[UZUNLUK - 1 : halfUZUNLUK];
+    localparam v2 = buyruk[halfUZUNLUK - 1 : 0]; 
+*/
+
+/*assign v1 = buyruk[UZUNLUK - 1 : UZUNLUK/2];
+assign v2 = buyruk[UZUNLUK/2  : 0];*/
+
+wire [(UZUNLUK/2) - 1:0] v1,v2;
 
 always@(*) begin
-    case(buyruk[UZUNLUK + 2: UZUNLUK -1])
+    v1 = buyruk[UZUNLUK - 1 : UZUNLUK/2];
+    v2 = buyruk[(UZUNLUK/2) - 1 : 0];
+    case(buyruk[UZUNLUK+2:UZUNLUK-1])
         3'b000: sonuc = v1 + v2;
         3'b001: sonuc = v1 - v2;
         3'b010: sonuc = v1 & v2;
--- labs/lab6/bibpTB.v
+++ labs/lab6/bibpTB.v
@@ -4,13 +4,13 @@ parameter UZUNLUK = 8;
 reg [UZUNLUK+2:0] buyruk;
 wire [UZUNLUK/2:0] sonuc;
 
-bibp uut(buyruk, sonuc);
+bibp #(.UZUNLUK(UZUNLUK)) uut(.buyruk(buyruk), .sonuc(sonuc));
 
 initial begin
     $dumpfile("vbibp.vcd");
     $dumpvars;
 
-    buyruk = 11'b000_0101_0101; #10;
+    buyruk = 11'b001_0101_0100; #10;
     $finish;
 end
 endmodule
