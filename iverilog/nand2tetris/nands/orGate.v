1cb96c9f9a9df4d3fda8ac992da004dfbad75112
k0rrluna <k0rr.luna1@gmail.com>
2024-12-02 02:29:57 +0300

    formatter test

--- iverilog/nand2tetris/nands/orGate.v
+++ iverilog/nand2tetris/nands/orGate.v
@@ -1,13 +1,13 @@
 module orGate (
-    input A_i,
-    input B_i,
+    input  A_i,
+    input  B_i,
     output F_o
 );
 
-wire nand1_out, nand2_out;
+   wire nand1_out, nand2_out;
 
-nand nand1(nand1_out, A_i, A_i);
-nand nand2(nand2_out, B_i, B_i);
-nand nand3(F_o, nand1_out, nand2_out);
+   nand nand1 (nand1_out, A_i, A_i);
+   nand nand2 (nand2_out, B_i, B_i);
+   nand nand3 (F_o, nand1_out, nand2_out);
 
 endmodule
