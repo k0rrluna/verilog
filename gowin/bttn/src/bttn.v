8e613a767e665f93461ad135c30bc216aed056a2
k0rrluna <k0rr.luna1@gmail.com>
2025-01-20 18:23:08 +0300

    tangFpga

--- gowin/bttn/src/bttn.v
+++ gowin/bttn/src/bttn.v
@@ -2,12 +2,18 @@ module bttn (
     input [3:0] A, B,
     input [2:0] opCodeA,
     input [1:0] select,
+    input Cin,
+    output [1:0] leds,
     output [11:0] Y
 );
 
+
 wire wire1, wire2;
 wire [11:0] selectY;
-ALU a1(.A(A), .B(B), .opCodeA(opCodeA), .CarryIN(1'b0), .bcd(selectY), .CarryOUT(wire1), .overflow(wire2));
+ALU a1(.A(A), .B(B), .opCodeA(opCodeA), .CarryIN(Cin), .bcd(selectY), .CarryOUT(wire2), .overflow(wire1));
 selector s1(.A(A), .B(B), .opCodeA(opCodeA), .select(select), .ALUY(selectY), .Y(Y));
+
+assign leds[0] = ~wire1;
+assign leds[1] = ~wire2;
 
 endmodule
